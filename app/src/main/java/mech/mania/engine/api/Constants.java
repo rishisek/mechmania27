ba284bbfde1ee810afe14867bd0cfa1502fac2ea
Shaw Kagawa <skagawa2@illinois.edu>
2021-03-13 15:44:57 -0600

    Commented out initializations in Constants

--- app/src/main/java/mech/mania/engine/api/Constants.java
+++ app/src/main/java/mech/mania/engine/api/Constants.java
@@ -1,9 +1,12 @@
 package mech.mania.engine.api;
 
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
 public final class Constants {
     // =========== BOARD CONSTANTS ===============
-    public final int boardHeight;
-    public final int boardWidth;
+    public final int BOARD_HEIGHT;
+    public final int BOARD_WIDTH;
 
     public final int fbandInitPosition;
     public final int fbandInitDelay;
@@ -13,42 +16,42 @@ public final class Constants {
     public final int fbandOuterHeight;
 
     // =========== GAME CONSTANTS ===============
-    public final int startingMoney;
-    public final int maxMovement;
-    public final int plantRadius;
-    public final int harvestRadius;
-    public final int carryingCapacity;
-    public final int protectionRadius;
+    public final int STARTING_MONEY;
+    public final int MAX_MOVEMENT;
+    public final int PLANT_RADIUS;
+    public final int HARVEST_RADIUS;
+    public final int CARRYING_CAPACITY;
+    public final int PROTECTION_RADIUS;
 
     public final Map<String, CropInformation> almanac;
 
     // CONSTRUCTORS
     /**
      * Constructor used for debug when you want to set your own constants.
-     * @param prefix of .properties file in the resources folder
+     * @param resourceName prefix of .properties file in the resources folder
      */
     public Constants(String resourceName) throws MissingResourceException {
         ResourceBundle rb = ResourceBundle.getBundle(resourceName);
 
         // board props
-        boardHeight =           Integer.parseInt(rb.getString("board.height"));
-        boardWidth =            Integer.parseInt(rb.getString("board.width"));
-        grassRows =             Integer.parseInt(rb.getString("board.grass.rows"));
-        greengrocerLength =     Integer.parseInt(rb.getString("board.greengrocer.length"));
-        fBandInnerHeight =      Integer.parseInt(rb.getString("fertilityband.inner.height"));
-        fBandMidHeight =        Integer.parseInt(rb.getString("fertilityband.mid.height"));
-        fBandOuterHeight =      Integer.parseInt(rb.getString("fertilityband.outer.height"));
-        fBandMoveDelay =        Integer.parseInt(rb.getString("fertilityband.speed"));
-        fBandInitDelay =        Integer.parseInt(rb.getString("fertilityband.delay"));
-        fBandInitPosition =     Integer.parseInt(rb.getString("fertilityband.start"));
+        BOARD_HEIGHT =           Integer.parseInt(rb.getString("board.height"));
+        BOARD_WIDTH =            Integer.parseInt(rb.getString("board.width"));
+        // grassRows =             Integer.parseInt(rb.getString("board.grass.rows"));
+        // greengrocerLength =     Integer.parseInt(rb.getString("board.greengrocer.length"));
+        // fBandInnerHeight =      Integer.parseInt(rb.getString("fertilityband.inner.height"));
+        // fBandMidHeight =        Integer.parseInt(rb.getString("fertilityband.mid.height"));
+        // fBandOuterHeight =      Integer.parseInt(rb.getString("fertilityband.outer.height"));
+        // fBandMoveDelay =        Integer.parseInt(rb.getString("fertilityband.speed"));
+        // fBandInitDelay =        Integer.parseInt(rb.getString("fertilityband.delay"));
+        // fBandInitPosition =     Integer.parseInt(rb.getString("fertilityband.start"));
 
         // player props
-        carryingCapacity =      Integer.parseInt(rb.getString("player.carrycapacity"));
-        maxMovement =           Integer.parseInt(rb.getString("player.maxmovement"));
-        plantRadius =           Integer.parseInt(rb.getString("player.plantradius"));
-        harvestRadius =         Integer.parseInt(rb.getString("player.harvestradius"));
-        protectionRadius =      Integer.parseInt(rb.getString("player.protectionradius"));
-        startingMoney =         Integer.parseInt(rb.getString("player.startingmoney"));
+        CARRYING_CAPACITY =      Integer.parseInt(rb.getString("player.carrycapacity"));
+        MAX_MOVEMENT =           Integer.parseInt(rb.getString("player.maxmovement"));
+        PLANT_RADIUS =           Integer.parseInt(rb.getString("player.plantradius"));
+        HARVEST_RADIUS =         Integer.parseInt(rb.getString("player.harvestradius"));
+        PROTECTION_RADIUS =      Integer.parseInt(rb.getString("player.protectionradius"));
+        STARTING_MONEY =         Integer.parseInt(rb.getString("player.startingmoney"));
 
         // TODO: set up almanac
         // almanac =
