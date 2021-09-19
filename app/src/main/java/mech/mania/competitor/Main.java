2841d196790e723ed77576a6ec5bbc6bab938051
nikwalia <nikash.walia@gmail.com>
2021-09-19 15:50:11 -0500

    Replace ItemDecision with NullDecision

--- app/src/main/java/mech/mania/competitor/Main.java
+++ app/src/main/java/mech/mania/competitor/Main.java
@@ -63,7 +63,7 @@ public class Main {
                 gameState.getTurn(),
                 String.join(", ", gameState.getFeedback())));
 
-        ActionDecision toSend = new UseItemDecision();
+        ActionDecision toSend = new NullDecision();
         logger.debug(String.format("[Turn %d] Sending ActionDecision: [%s]",
                 gameState.getTurn(), toSend));
         return toSend;
