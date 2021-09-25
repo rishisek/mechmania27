a9c3f0369c5163a6cf07343f289c129cbef1c311
Rishi Sekhar <rishi.sek@gmail.com>
2021-09-25 01:27:31 -0500

    Change Strategy interface

--- app/src/main/java/mech/mania/competitor/mm_strategies/Strategy.java
+++ app/src/main/java/mech/mania/competitor/mm_strategies/Strategy.java
@@ -1,10 +1,11 @@
 package mech.mania.competitor.mm_strategies;
 
 import mech.mania.competitor.Game;
+import mech.mania.competitor.mm_core.Manager;
 import mech.mania.competitor.model.decisions.ActionDecision;
 import mech.mania.competitor.model.decisions.MoveDecision;
 
 abstract public class Strategy {
-  abstract MoveDecision getMoveDecision(Game game);
-  abstract ActionDecision getActionDecision(Game game);
+  abstract MoveDecision getMoveDecision(Manager manager);
+  abstract ActionDecision getActionDecision(Manager manager);
 }
