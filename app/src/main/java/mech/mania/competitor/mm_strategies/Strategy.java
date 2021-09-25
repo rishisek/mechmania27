5f449dc806a0f0482b7ef2258c8061fb4aee62d6
Rishi Sekhar <rsekhar2@illinois.edu>
2021-09-25 01:01:30 -0500

    Add Strategy abstract methods

--- app/src/main/java/mech/mania/competitor/mm_strategies/Strategy.java
+++ app/src/main/java/mech/mania/competitor/mm_strategies/Strategy.java
@@ -1,5 +1,10 @@
 package mech.mania.competitor.mm_strategies;
 
-abstract public class Strategy {
+import mech.mania.competitor.Game;
+import mech.mania.competitor.model.decisions.ActionDecision;
+import mech.mania.competitor.model.decisions.MoveDecision;
 
+abstract public class Strategy {
+  abstract MoveDecision getMoveDecision(Game game);
+  abstract ActionDecision getActionDecision(Game game);
 }
