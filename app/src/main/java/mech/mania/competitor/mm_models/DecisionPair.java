55d85111f802895fa0f54d04c0600595c387bccc
Rishi <rishi.sek@gmail.com>
2021-09-25 02:30:51 -0500

    Add DecisionPair constructor

--- app/src/main/java/mech/mania/competitor/mm_models/DecisionPair.java
+++ app/src/main/java/mech/mania/competitor/mm_models/DecisionPair.java
@@ -6,4 +6,9 @@ import mech.mania.competitor.model.decisions.MoveDecision;
 public class DecisionPair {
   public MoveDecision moveDecision;
   public ActionDecision actionDecision;
+
+  public DecisionPair(MoveDecision moveDecision, ActionDecision actionDecision) {
+    this.moveDecision = moveDecision;
+    this.actionDecision = actionDecision;
+  }
 }
